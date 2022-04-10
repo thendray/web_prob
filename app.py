ac340541b4e7b9b8103af263b000e36227140263
Андрей <and.rey8o79@yandex.ru>
2022-04-10 18:06:29 +0300

    add registration/authorisation error messages

--- app.py
+++ app.py
@@ -1,58 +1,125 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash, get_flashed_messages
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from werkzeug import security
 
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:Time_to_fly@localhost/web_prob"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['SECRET_KEY'] = "asdfgrtyeofdhggkjdjfh09586"
 db = SQLAlchemy(app)
 
+current_user = None
 
-class Information(db.Model):
-    __tablename__ = "web_prob"
 
-    def __init__(self, name: str, telephone: str, address: str = ""):
+class User(db.Model):
+    #__tablename__ = "Users"
+
+    def __init__(self, name: str, login: str, password: str = ""):
 
         self.name = name
-        self.telephone = telephone
-        self.address = address
+        self.login = login
+        self.password = password
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column("name", db.String(100), nullable=False)
-    telephone = db.Column("telephone", db.String(20), nullable=True)
-    address = db.Column("address", db.String(200))
+    name = db.Column("name", db.String(50), nullable=False)
+    login = db.Column("login", db.String(50), nullable=True)
+    password = db.Column("password", db.String(500))
 
     def __repr__(self):
         return f"<Information> {self.id}"
 
 
+class TextFile(db.Model):
+
+    def __init__(self, file_name: str = None, text: str = None):
+
+        self.text = text
+        self.file_name = file_name
+        self.create_datetime = datetime.now()
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    file_name = db.Column("file_name", db.String(100), nullable=False)
+    text = db.Column("text", db.String(500), nullable=True)
+    create_datetime = db.Column("create_datetime", db.DateTime, nullable=False)
+
+    def __repr__(self):
+        return f"<TextFiles> {self.id}"
+
+
 @app.route("/")
 def hello():
-    return render_template("home.html")
+    return render_template("home.html", current_user=current_user)
 
 
-@app.route("/form", methods=["GET", "POST"])
-def form():
+@app.route("/registration", methods=["GET", "POST"])
+def registration():
+    global current_user
     if request.method == "POST":
         name = request.form["name"]
-        telephone = request.form["telephone"]
-        address = request.form["address"]
+        login = request.form["login"]
+        password = request.form["password"]
+        confirm_password = request.form['confirm_password']
 
-        info = Information(name, telephone, address)
+        if confirm_password != password:
+            # Вернуть ту же страничку с ошибкой не правильного пароля
+            flash("Пароли не совпадают!", 'error')
+            return redirect("/registration")
+
+        elif User.query.filter(User.login == login).count() > 0:
+            # Вернуть ту же страничку с ошибкой не уникального login
+            flash("Такой логин уже существует!", 'error')
+            return redirect("/registration")
+
+        password = security.generate_password_hash(password)
+        user = User(name, login, password)
 
         try:
-            db.session.add(info)
+            db.session.add(user)
             db.session.commit()
+            # Вернуть домашнюю страничку авторизованного пользователя
+            current_user = user
             return redirect("/")
-        except:
-            return "SOS"
-    return render_template("form.html")
+        except Exception as error:
+            return str(error)
+
+    return render_template("registration_form.html")
 
 
-@app.route("/result", methods=["POST", "GET"])
-def result():
-    info = Information.query.order_by(Information.name).all()
-    return render_template("results.html", info=info)
+@app.route("/enter", methods=["GET", "POST"])
+def enter():
+    global current_user
+    if request.method == "POST":
+        login = request.form["login"]
+        password = request.form["password"]
+
+        check = User.query.filter(security.generate_password_hash(password) == User.password and User.login == login)
+
+        if check.count() > 0:
+            current_user = check.first()
+            # Вернуть домашнюю страницу авторизованного пользователя
+            return redirect("/")
+        else:
+            # Вернуть ту же страницу с сообщением
+            flash("Пользователь не найден!", "error")
+            return redirect("/enter")
+
+    return render_template("enter_form.html")
+
+
+@app.route("/exit")
+def sign_out():
+    global current_user
+    current_user = None
+    return redirect('/')
+
+
+# @app.route("/result", methods=["POST", "GET"])
+# def result():
+#     info = Information.query.order_by(Information.name).all()
+#     return render_template("results.html", info=info)
 
 
 if __name__ == "__main__":
